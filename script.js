68457924b7cff163583c609c0a61d0041c2a5f94
IDarwinOrtizI <sebastiancaballero0811@gmail.com>
2025-11-16 15:11:19 -0500

    ToDo

---
+++ script.js
@@ -0,0 +1,44 @@
+const Input = document.getElementById("textInput");
+const agregarBtn = document.getElementById("botonAgregar");
+const Lista = document.getElementById("lista");
+
+let item;
+
+function agregarTarea() {
+    const nuevaTarea = Input.value.trim(); 
+
+    if (nuevaTarea !== "") {  
+        item = document.createElement("li");
+        item.innerHTML = `<span>${nuevaTarea}</span>
+                        <button class="delete-btn">Eliminar</button>`;
+
+        const eliminarBtn = item.querySelector(".delete-btn");
+        eliminarBtn.addEventListener("click", eliminarTarea);
+        Lista.appendChild(item);
+        Input.value = ""; 
+    }
+}
+
+function eliminarTarea() {
+    this.parentElement.remove();
+}
+
+agregarBtn.addEventListener("click", agregarTarea); 
+
+Input.addEventListener("keypress", function (e) {
+    if (e.key === "Enter") {
+        agregarTarea();
+    }
+});
+
+Lista.addEventListener("click", function (e) {
+    if (e.target.tagName === "SPAN") {
+        e.target.classList.toggle("complete"); 
+    }
+});
+
+Lista.addEventListener("click", function (e) {
+    if (e.target.classList.contains("delete-btn")) {  
+        e.target.parentElement.remove();
+    }
+});
